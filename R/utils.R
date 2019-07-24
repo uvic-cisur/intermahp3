823169bd384ad174c615ea769b679b1fdf2ea3fc
Samuel Churchill <meadowschurchill@gmail.com>
2019-07-24 14:50:02 -0700

    Prepping new RR datasets

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+#### Imports -------------------------------------------------------------------
+
+#' @importFrom magrittr %>% %<>%
+#' @importFrom dplyr
+#' mutate filter group_by ungroup inner_join select bind_rows left_join
+#' @importFrom purrr map pmap map2_dbl map_dbl map2
+#' @keywords internal
+foo <- function() {return("bar")}
---
+++ README.md
@@ -0,0 +1,3 @@
+# The International Model of Alcohol Harms and Policies R Backend
+
+> *Copyright 2019 Canadian Institute for Substance Use Research. Licensed under the MIT license.*
